b6a6344e46e03f9f878bf1a97d7496fb501fb8f4
Saba Gotsiridze <Gotsiridze.S@outlook.com>
2021-10-13 21:25:54 +0400

    Add GetCommandsForPlatform Controller

--- CommandService/Controllers/CommandsController.cs
+++ CommandService/Controllers/CommandsController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using CommandService.Data;
+using CommandService.DataTransferObjects;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,11 +10,25 @@ using System.Threading.Tasks;
 namespace CommandService.Controllers
 {
     [ApiController]
+    [Route("api/c/platforms/{platformId}/[controller]")]
     public class CommandsController : ControllerBase
     {
-        public CommandsController()
-        {
+        private readonly ICommandRepository _repository;
+        private readonly IMapper _mapper;
 
+        public CommandsController(ICommandRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
         }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<CommandReadDto>> GetCommandsForPlatform(int platformId) =>
+            _repository.PlatformExists(platformId) switch
+            {
+                true => Ok(_mapper.Map<IEnumerable<CommandReadDto>>(_repository.GetCommandsForPlatform(platformId))),
+                false => NotFound()
+            };
+
     }
 }
