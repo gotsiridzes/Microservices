19b30ca8c2cd6f2af2c1e309f5b7aab3b23f393d
Saba Gotsiridze <Gotsiridze.S@outlook.com>
2021-10-13 21:11:43 +0400

    Add Data Transfer Objects Add Automapper

---
+++ CommandService/DataTransferObjects/CommandCreateDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommandService.DataTransferObjects
+{
+    public class CommandCreateDto
+    {
+        [Required]
+        public string HowTo { get; set; }
+
+        [Required]
+        public string CommandLine { get; set; }
+    }
+}
---
+++ CommandService/DataTransferObjects/PlatformReadDto.cs
@@ -0,0 +1,8 @@
+﻿namespace CommandService.DataTransferObjects
+{
+    public class PlatformReadDto
+    {
+        public int Id { get; set; }
+
+    }
+}
