a3e620672e4635e632c045655bf980d1f1bc0b46
Saba Gotsiridze <Gotsiridze.S@outlook.com>
2021-10-13 20:57:59 +0400

    Add and implement CommandRepository

---
+++ CommandService/Data/CommandRepository.cs
@@ -0,0 +1,60 @@
+﻿using CommandService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CommandService.Data
+{
+    public class CommandRepository : ICommandRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public CommandRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        #region Commands
+
+        public void CreateCommand(int platformId, Command command)
+        {
+            if (command is null)
+                throw new ArgumentNullException(nameof(command));
+            else
+            {
+                command.PlatformId = platformId;
+                _dbContext.Commands.Add(command);
+            }
+        }
+
+        public Command GetCommand(int platformId, int commandId) =>
+            _dbContext.Commands
+            .Where(x => x.PlatformId == platformId && x.Id == commandId)
+            .FirstOrDefault();
+
+        public IEnumerable<Command> GetCommandsForPlatform(int platformId) => 
+            _dbContext.Commands
+            .Where(x => x.PlatformId == platformId);
+        #endregion
+
+        #region Platforms
+
+        public void CreatePlatform(Platform platform)
+        {
+            if (platform is null)
+                throw new ArgumentNullException(nameof(platform));
+            else
+                _dbContext.Platforms.Add(platform);
+        }
+
+        public IEnumerable<Platform> GetPlatforms() => _dbContext.Platforms.ToList();
+
+        public bool PlatformExists(int platformId) => 
+            _dbContext.Platforms
+            .Any(x => x.Id == platformId);
+
+        #endregion
+
+        public bool SaveChanges() => _dbContext.SaveChanges() > 0;
+    }
+}
---
+++ CommandService/Data/ICommandRepository.cs
@@ -0,0 +1,27 @@
+﻿using CommandService.Models;
+using System.Collections.Generic;
+
+namespace CommandService.Data
+{
+    public interface ICommandRepository
+    {
+        bool SaveChanges();
+        
+        #region Platforms
+
+        IEnumerable<Platform> GetPlatforms();
+        void CreatePlatform(Platform platform);
+        bool PlatformExists(int platformId);
+
+        #endregion
+
+        #region Commands
+
+        IEnumerable<Command> GetCommandsForPlatform(int platformId);
+        Command GetCommand(int platformId, int commandId);
+        void CreateCommand(int platformId, Command command);
+        
+        #endregion
+
+    }
+}
