4696ab8bf15eac90ca3d02299752f51f3693e258
Saba Gotsiridze <Gotsiridze.S@outlook.com>
2021-10-13 20:40:22 +0400

    CommandsService: Add Command and Platform models

---
+++ CommandService/Models/Platform.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace CommandService.Models
+{
+    public class Platform
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+        
+        [Required]
+        public int ExternalId { get; set; }
+        
+        public ICollection<Command> Commands { get; set; } = new List<Command>();
+    }
+}
