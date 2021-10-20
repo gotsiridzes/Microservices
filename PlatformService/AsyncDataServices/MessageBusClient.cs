2978cbc51b26d90685bb36d77aefa47510076296
Saba Gotsiridze <Gotsiridze.S@outlook.com>
2021-10-20 20:48:08 +0400

    Establish Connection to message bus

---
+++ PlatformService/AsyncDataServices/MessageBusClient.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using PlatformService.DataTransferObjects;
+using RabbitMQ.Client;
+using System;
+
+namespace PlatformService.AsyncDataServices
+{
+    public class MessageBusClient : IMessageBusClient
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IConnection _connection;
+        private readonly IModel _channel;
+
+        public MessageBusClient(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            var factory = new ConnectionFactory()
+            {
+                HostName = _configuration["RabbitMQHost"],
+                Port = int.Parse(_configuration["RabbitMQPort"])
+            };
+
+            try
+            {
+                _connection = factory.CreateConnection();
+                _channel = _connection.CreateModel();
+                _channel.ExchangeDeclare(exchange: "trigger", type: ExchangeType.Fanout);
+                _connection.ConnectionShutdown += RabbitMq_ConnectionShutDown;
+                
+                Console.WriteLine("Connected To Message Bus");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Could Not Connect to the Message Bus: {ex}");
+            }
+        }
+
+        public void PublishNewPlatform(PlatformPublishedDto platformPublishedDto)
+        {
+            
+        }
+
+        private void RabbitMq_ConnectionShutDown(object sender, ShutdownEventArgs e)
+        {
+            Console.WriteLine("RabbitMQ Connection Shut Down");
+        }
+    }
+}
