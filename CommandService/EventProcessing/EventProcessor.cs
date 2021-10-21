a5df80de0eee57780caa12d30887ab583089c895
Saba Gotsiridze <Gotsiridze.S@outlook.com>
2021-10-21 19:42:30 +0400

    DetermineEvent

---
+++ CommandService/EventProcessing/EventProcessor.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using CommandService.DataTransferObjects;
+using Microsoft.Extensions.DependencyInjection;
+using System.Text.Json;
+
+namespace CommandService.EventProcessing
+{
+    public enum EventType
+    {
+        PlatformPublished,
+        Undetermined
+    }
+
+    public class EventProcessor : IEventProcessor
+    {
+        private readonly IServiceScopeFactory _scopeFactory;
+        private readonly IMapper _mapper;
+
+        public EventProcessor(
+            IServiceScopeFactory scopeFactory,
+            IMapper mapper)
+        {
+            _scopeFactory = scopeFactory;
+            _mapper = mapper;
+        }
+
+        public void ProcessEvent(string message)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        private EventType DetermineEvent(string notificationMessage)
+        {
+            System.Console.WriteLine("Determining Event");
+            var eventType = JsonSerializer.Deserialize<GenericEventDto>(notificationMessage);
+            switch (eventType.Event)
+            {
+                case "Platform_Published":
+                    System.Console.WriteLine("Platform_Published Event Detected");
+                    return EventType.PlatformPublished;
+                default:
+                    System.Console.WriteLine("Could not determine the event type");
+                    return EventType.Undetermined;
+            }
+        }
+    }
+}
